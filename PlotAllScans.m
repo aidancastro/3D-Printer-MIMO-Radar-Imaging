fb7ec0d65282368ffa3201b41c9f5be2d5659691
Aidan Castro <castroaidan0420@gmail.com>
2025-09-11 19:10:31 -0400

    -fixed input variables and added expected variables -works for all 2d scans with singleton dimension

--- PlotAllScans.m
+++ PlotAllScans.m
@@ -1,11 +1,13 @@
 
 %load reconstructed file
+%expects xgrid,ygrid,zgrid, y_accum, 
 load('reconstructed_ball.mat');
 
 figure();
-nRecs = numel(recs(1,1,:));
+nRecs = numel(y_accum(1,1,:));
 
 for i=1:nRecs:1
+y_cart = y_accum(:,:,i);
 %% Plot Y-Z Slice
         if and(min([length(ygrid),length(zgrid)])>2,length(xgrid)<=10)
             subplot(nRecs/5,nRecs/5, i)
@@ -14,16 +16,17 @@ for i=1:nRecs:1
             set(ax,'EdgeColor', 'none');
             xlabel('Y [m]'); ylabel('Z [m]');
             title(sprintf("YZ Scan %d", i));
-            filename = sprintf('YZ_2D_%s', dateTag);
+            
 
         end
 %% Plot X-Z Slice
         if and(min([length(xgrid),length(zgrid)])>2,length(ygrid)<=10)
+            subplot(nRecs/5,nRecs/5, i)
             y_xz = 20*log10(rssq(y_cart(find(ygrid>=ygrid(1),1):find(ygrid>=ygrid(end),1),:,:),1));
-            figure(fig(3));ax=pcolor(squeeze(Xgrid(1,:,:)),squeeze(Zgrid(1,:,:)),squeeze(y_xz));
+            ax=pcolor(squeeze(Xgrid(1,:,:)),squeeze(Zgrid(1,:,:)),squeeze(y_xz));
             set(ax,'EdgeColor', 'none');
             xlabel('X [m]'); ylabel('Z [m]');
-            filename = sprintf('XZ_2D_%s', dateTag);
+            
             % if first_iter 
             %     set(gca,'NextPlot','replacechildren');
             %     title('xz view');xlabel('x');ylabel('z');daspect([1,1,1]);%caxis([-20,20]);
@@ -32,10 +35,11 @@ for i=1:nRecs:1
 
 %% Plot X-Y Slice
         if and(min([length(xgrid),length(ygrid)])>2,length(zgrid)<=10)
-           % y_xy = 20*log10(rssq(y_cart(:,:,find(zgrid>=zgrid(1),1):find(zgrid>=zgrid(end),1)),2));
-            figure(fig(2));ax = pcolor( squeeze(Xgrid(:,:,1)), squeeze(Ygrid(:,:,1)), ...
+            subplot(nRecs/5,nRecs/5, i)
+            y_xy = 20*log10(rssq(y_cart(find(zgrid>=zgrid(1),1):find(zgrid>=zgrid(end),1)),2));
+            ax = pcolor( squeeze(Xgrid(:,:,1)), squeeze(Ygrid(:,:,1)), ...
              20*log10(rssq(y_cart(:,:,find(zgrid>=zgrid(1),1):find(zgrid>=zgrid(end),1)),3)) );
-            filename = sprintf('XY_2D_%s', dateTag);
+            
             % if first_iter
             %     set(gca,'NextPlot','replacechildren');
             %     title('xy view');xlabel('x');ylabel('y');daspect([1,1,1]);%caxis([-20,20]);
